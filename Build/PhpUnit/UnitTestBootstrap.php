545d663f170b952395b117bbee268497c787b698
Andreas Wolf <aw@foundata.net>
2014-08-12 17:12:13 +0200

    [CONFIG] Unit test configuration and bootstrap

---
+++ Build/PhpUnit/UnitTestBootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow build system.                    *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+$composerAutoloader = __DIR__ . '/../../vendor/autoload.php';
+if(!file_exists($composerAutoloader)) {
+	exit(PHP_EOL . 'Bootstrap Error: The unit test bootstrap requires the autoloader file created at install time by Composer. Looked for "' . $composerAutoloader . '" without success.');
+}
+require_once($composerAutoloader);
