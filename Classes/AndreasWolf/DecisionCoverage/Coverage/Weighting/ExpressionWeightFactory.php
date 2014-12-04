97ee092ea45e17fb5bb207820b9248fb6cbe5ee3
Andreas Wolf <aw@foundata.net>
2014-12-04 14:23:58 +0100

    [FEATURE] Create decision weights

--- Classes/AndreasWolf/DecisionCoverage/Coverage/Weighting/ExpressionWeightFactory.php
+++ Classes/AndreasWolf/DecisionCoverage/Coverage/Weighting/ExpressionWeightFactory.php
@@ -11,12 +11,36 @@ use PhpParser\Node\Expr;
  */
 class ExpressionWeightFactory {
 
+	protected $decisionTypeToWeightClassMap = array(
+		'Expr_BinaryOp_BooleanAnd' => 'BooleanAndWeight',
+		'Expr_BinaryOp_BooleanOr' => 'BooleanOrWeight',
+	);
+
 	/**
 	 * @param Expr $expr
-	 * @return ExpressionWeight
+	 * @return ConditionWeight
 	 */
 	public function createForCondition(Expr $expr) {
 		return new ConditionWeight();
+	}
+
+	/**
+	 * @param Expr\BinaryOp $expr
+	 * @param ExpressionWeight $leftWeight
+	 * @param ExpressionWeight $rightWeight
+	 * @return DecisionWeight
+	 */
+	public function createForDecision(Expr\BinaryOp $expr, ExpressionWeight $leftWeight, ExpressionWeight $rightWeight) {
+		if (!array_key_exists($expr->getType(), $this->decisionTypeToWeightClassMap)) {
+			throw new \InvalidArgumentException('No decision weighting available for type ' . $expr->getType(), 1417689417);
+		}
+		$type = 'AndreasWolf\\DecisionCoverage\\Coverage\\Weighting\\'
+			. $this->decisionTypeToWeightClassMap[$expr->getType()];
+
+		/** @var DecisionWeight $decisionWeight */
+		$decisionWeight = new $type($leftWeight, $rightWeight);
+
+		return $decisionWeight;
 	}
 
 }
--- Tests/Unit/Coverage/Weighting/ExpressionWeightFactoryTest.php
+++ Tests/Unit/Coverage/Weighting/ExpressionWeightFactoryTest.php
@@ -3,6 +3,7 @@ namespace AndreasWolf\DecisionCoverage\Tests\Unit\Coverage\Weighting;
 
 use AndreasWolf\DecisionCoverage\Coverage\Weighting\ExpressionWeightFactory;
 use AndreasWolf\DecisionCoverage\Tests\Unit\UnitTestCase;
+use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
 
 
 class ExpressionWeightFactoryTest extends UnitTestCase {
@@ -16,6 +17,36 @@ class ExpressionWeightFactoryTest extends UnitTestCase {
 		$weight = $subject->createForCondition($this->getMock('PhpParser\Node\Expr'));
 
 		$this->assertInstanceOf('AndreasWolf\DecisionCoverage\Coverage\Weighting\ConditionWeight', $weight);
+	}
+
+	/**
+	 * @test
+	 */
+	public function weightObjectForBooleanAndIsInstanceOfRightClass() {
+		$subject = new ExpressionWeightFactory();
+
+		$expr = new BooleanAnd($this->getMock('PhpParser\Node\Expr'), $this->getMock('PhpParser\Node\Expr'));
+
+		$weight = $subject->createForDecision($expr, $this->mockWeight(), $this->mockWeight());
+
+		$this->assertInstanceOf('AndreasWolf\DecisionCoverage\Coverage\Weighting\BooleanAndWeight', $weight);
+	}
+
+	/**
+	 * @test
+	 */
+	public function creatingDecisionWeightObjectForExpressionThatIsNoDecisionLeadsToFailure() {
+		$this->setExpectedException('InvalidArgumentException', '', 1417689417);
+		$subject = new ExpressionWeightFactory();
+
+		$expr = $this->getMockBuilder('PhpParser\Node\Expr\BinaryOp')->disableOriginalConstructor()->getMock();
+
+		$weight = $subject->createForDecision($expr, $this->mockWeight(), $this->mockWeight());
+	}
+
+
+	protected function mockWeight() {
+		return $this->getMockBuilder('AndreasWolf\DecisionCoverage\Coverage\Weighting\ExpressionWeight')->getMock();
 	}
 
 }
