45e24461ab4934e560ff0b0befc119212186e9c4
Andreas Wolf <aw@foundata.net>
2014-12-10 19:09:45 +0100

    [FEATURE] Coupling classification
    This adds the classifier together with the first few test for same-type
    and similar-type classifications.

---
+++ Classes/AndreasWolf/DecisionCoverage/Coverage/Coupling/CouplingClassifier.php
@@ -0,0 +1,126 @@
+<?php
+namespace AndreasWolf\DecisionCoverage\Coverage\Coupling;
+
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar\LNumber;
+
+
+/**
+ *
+ *
+ * This class uses a few definitions for its work:
+ *
+ * - "similar" relations are: > and >=, < and <=
+ *
+ * @author Andreas Wolf <aw@foundata.net>
+ *
+ * TODO make this work independent of the ordering within a condition, i.e. $a < 5 and 5 > $a should be treated equal
+ */
+class CouplingClassifier {
+
+	protected $similarTypes = array(
+		array('Expr_BinaryOp_Greater', 'Expr_BinaryOp_GreaterOrEqual'),
+		array('Expr_BinaryOp_Smaller', 'Expr_BinaryOp_SmallerOrEqual')
+	);
+
+	public function determineConditionCoupling(Expr\BinaryOp $leftExpression, Expr\BinaryOp $rightExpression) {
+		if (!$this->doReferenceSameVariable($leftExpression, $rightExpression)) {
+			return ConditionCoupling::TYPE_UNCOUPLED;
+		} else {
+			if ($leftExpression->getType() == $rightExpression->getType()) {
+				return $this->determineCouplingOfSameTypeConditions($leftExpression, $rightExpression);
+			} elseif ($this->haveSimilarTypes($leftExpression, $rightExpression)) {
+				return $this->determineCouplingOfSimilarTypeConditions($leftExpression, $rightExpression);
+			}
+		}
+		return NULL;
+	}
+
+	/**
+	 * Tests if expressions
+	 *
+	 * @param Expr\BinaryOp $leftExpression
+	 * @param Expr\BinaryOp $rightExpression
+	 * @return bool
+	 */
+	protected function doReferenceSameVariable(Expr\BinaryOp $leftExpression, Expr\BinaryOp $rightExpression) {
+		$leftVariable = $leftExpression->left instanceof Expr\Variable ? $leftExpression->left : $leftExpression->right;
+		$rightVariable = $rightExpression->left instanceof Expr\Variable ? $rightExpression->left : $rightExpression->right;
+
+		return ($leftVariable->name === $rightVariable->name);
+	}
+
+	protected function determineCouplingOfSameTypeConditions(Expr\BinaryOp $leftExpression,
+	                                                         Expr\BinaryOp $rightExpression) {
+		$leftValue = $this->extractConstantValue($leftExpression->right);
+		$rightValue = $this->extractConstantValue($rightExpression->right);
+
+		// same variable and same value means the conditions are effectively the same
+		if ($leftValue == $rightValue) {
+			return ConditionCoupling::TYPE_IDENTICAL;
+		}
+
+		switch ($leftExpression->getType()) {
+			case 'Expr_BinaryOp_Greater':
+			case 'Expr_BinaryOp_GreaterOrEqual':
+				if ($leftValue < $rightValue) {
+					return ConditionCoupling::TYPE_SUPERSET;
+				} elseif ($leftValue > $rightValue) {
+					return ConditionCoupling::TYPE_SUBSET;
+				} // equal values were handled above
+				break;
+
+			case 'Expr_BinaryOp_Smaller':
+			case 'Expr_BinaryOp_SmallerOrEqual':
+				if ($leftValue > $rightValue) {
+					return ConditionCoupling::TYPE_SUPERSET;
+				} elseif ($leftValue < $rightValue) {
+					return ConditionCoupling::TYPE_SUBSET;
+				} // equal values were handled above
+		}
+		return NULL; // this should never happen
+	}
+
+	protected function determineCouplingOfSimilarTypeConditions(Expr\BinaryOp $leftExpression,
+	                                                            Expr\BinaryOp $rightExpression) {
+		$leftValue = $this->extractConstantValue($leftExpression->right);
+		$rightValue = $this->extractConstantValue($rightExpression->right);
+
+		switch ($leftExpression->getType()) {
+			case 'Expr_BinaryOp_GreaterOrEqual':
+				// the right expression is a "greater"
+				if ($leftValue == $rightValue) {
+					return ConditionCoupling::TYPE_SUPERSET;
+				}
+
+				break;
+
+			case 'Expr_BinaryOp_SmallerOrEqual':
+				// the right expression is a "smaller"
+				if ($leftValue == $rightValue) {
+					return ConditionCoupling::TYPE_SUPERSET;
+				}
+
+				break;
+		}
+		return NULL; // this should never happen
+	}
+
+	protected function haveSimilarTypes(Expr\BinaryOp $left, Expr\BinaryOp $right) {
+		return
+			(in_array($left->getType(), $this->similarTypes[0]) && in_array($right->getType(), $this->similarTypes[0]))
+			|| (in_array($left->getType(), $this->similarTypes[1]) && in_array($right->getType(), $this->similarTypes[1]));
+	}
+
+	/**
+	 * @param Expr $constantExpression
+	 * @return Expr
+	 */
+	protected function extractConstantValue(Expr $constantExpression) {
+		if ($constantExpression instanceof LNumber) {
+			return $constantExpression->value;
+		}
+	}
+
+}
---
+++ Tests/Unit/Coverage/Coupling/CouplingClassifierTest.php
@@ -0,0 +1,165 @@
+<?php
+namespace AndreasWolf\DecisionCoverage\Tests\Unit\Coverage\Coupling;
+use AndreasWolf\DecisionCoverage\Coverage\Coupling\ConditionCoupling;
+use AndreasWolf\DecisionCoverage\Coverage\Coupling\CouplingClassifier;
+use AndreasWolf\DecisionCoverage\Tests\Unit\UnitTestCase;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar;
+
+
+/**
+ *
+ *
+ * @author Andreas Wolf <aw@foundata.net>
+ */
+class CouplingClassifierTest extends UnitTestCase {
+
+	/**
+	 * @test
+	 */
+	public function unrelatedExpressionsAreConsideredUncoupled() {
+		$subject = new CouplingClassifier();
+
+		$coupling = $subject->determineConditionCoupling(
+			$this->mockGreater($this->mockVariable('a'), $this->mockInteger(5)),
+			$this->mockGreater($this->mockVariable('b'), $this->mockInteger(0))
+		);
+
+		$this->assertEquals(ConditionCoupling::TYPE_UNCOUPLED, $coupling, 'Conditions are not recognized as uncoupled.');
+	}
+
+	public function identicalExpressionsDataProvider() {
+		return array(
+			'greater' => array($this->mockGreater($this->mockVariable('a'), $this->mockInteger(5))),
+			'greater or equal' => array($this->mockGreaterOrEqual($this->mockVariable('a'), $this->mockInteger(5))),
+			'smaller' => array($this->mockSmaller($this->mockVariable('a'), $this->mockInteger(5))),
+			'smaller or equal' => array($this->mockSmallerOrEqual($this->mockVariable('a'), $this->mockInteger(5))),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider identicalExpressionsDataProvider
+	 */
+	public function identicalExpressionsAreClassifiedCorrectly($expression) {
+		$subject = new CouplingClassifier();
+
+		$coupling = $subject->determineConditionCoupling(
+			$expression,
+			$expression
+		);
+
+		$this->assertEquals(ConditionCoupling::TYPE_IDENTICAL, $coupling, 'Conditions are not recognized as identical.');
+	}
+
+	/**
+	 * The variables returned by this provider can be used for both testing super- and subset relations by switching
+	 * the two expressions (due to the antisymmetrical nature of these relation types).
+	 *
+	 * @return array
+	 */
+	public function sameTypeRelationalExpressionProvider() {
+		return array(
+			'greater than' => array(
+				$this->mockGreater($this->mockVariable('a'), $this->mockInteger(5)),
+				$this->mockGreater($this->mockVariable('a'), $this->mockInteger(10)),
+			),
+			'greater or equal' => array(
+				$this->mockGreaterOrEqual($this->mockVariable('a'), $this->mockInteger(5)),
+				$this->mockGreaterOrEqual($this->mockVariable('a'), $this->mockInteger(10)),
+			),
+			'smaller than' => array(
+				$this->mockSmaller($this->mockVariable('a'), $this->mockInteger(10)),
+				$this->mockSmaller($this->mockVariable('a'), $this->mockInteger(5)),
+			),
+			'smaller or equal' => array(
+				$this->mockSmallerOrEqual($this->mockVariable('a'), $this->mockInteger(10)),
+				$this->mockSmallerOrEqual($this->mockVariable('a'), $this->mockInteger(5)),
+			),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider sameTypeRelationalExpressionProvider
+	 */
+	public function relationalSupersetExpressionsWithSameTypeAndVariableAreCorrectlyClassified($leftExpression, $rightExpression) {
+		$subject = new CouplingClassifier();
+
+		$coupling = $subject->determineConditionCoupling($leftExpression, $rightExpression);
+
+		$this->assertEquals(ConditionCoupling::TYPE_SUPERSET, $coupling, 'Condition coupling is not recognized as superset');
+	}
+
+	/**
+	 * @test
+	 * @dataProvider sameTypeRelationalExpressionProvider
+	 */
+	public function relationalSubsetExpressionsWithSameTypeAndVariableAreCorrectlyClassified($rightExpression, $leftExpression) {
+		$subject = new CouplingClassifier();
+
+		$coupling = $subject->determineConditionCoupling($leftExpression, $rightExpression);
+
+		$this->assertEquals(ConditionCoupling::TYPE_SUBSET, $coupling, 'Condition coupling is not recognized as subset');
+	}
+
+	/**
+	 * The variables returned by this provider can be used for both testing super- and subset relations by switching
+	 * the two expressions (due to the antisymmetrical nature of these relation types).
+	 *
+	 * The two relational operators >/>= and </<= form a super-/subset relation when fed with the same values as the
+	 * one that includes "equals" also includes the compared value, while the other excludes it.
+	 *
+	 * @return array
+	 */
+	public function similarTypeAndSameValueRelationalExpressionProvider() {
+		return array(
+			'greater or equal and greater' => array(
+				$this->mockGreaterOrEqual($this->mockVariable('a'), $this->mockInteger(5)),
+				$this->mockGreater($this->mockVariable('a'), $this->mockInteger(5)),
+			),
+			'smaller or equal and smaller' => array(
+				$this->mockSmallerOrEqual($this->mockVariable('a'), $this->mockInteger(5)),
+				$this->mockSmaller($this->mockVariable('a'), $this->mockInteger(5)),
+			),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider similarTypeAndSameValueRelationalExpressionProvider
+	 */
+	public function supersetIsCorrectlyClassifiedForSameVariableValue($leftExpression, $rightExpression) {
+		$subject = new CouplingClassifier();
+
+		$coupling = $subject->determineConditionCoupling($leftExpression, $rightExpression);
+
+		$this->assertEquals(ConditionCoupling::TYPE_SUPERSET, $coupling, 'Condition coupling is not recognized as superset');
+	}
+
+
+	protected function mockGreater($left, $right) {
+		return new Expr\BinaryOp\Greater($left, $right);
+	}
+
+	protected function mockGreaterOrEqual($left, $right) {
+		return new Expr\BinaryOp\GreaterOrEqual($left, $right);
+	}
+
+	protected function mockSmaller($left, $right) {
+		return new Expr\BinaryOp\Smaller($left, $right);
+	}
+
+	protected function mockSmallerOrEqual($left, $right) {
+		return new Expr\BinaryOp\SmallerOrEqual($left, $right);
+	}
+
+	protected function mockVariable($variableName) {
+		return new Expr\Variable($variableName);
+	}
+
+	protected function mockInteger($value) {
+		return new Scalar\LNumber($value);
+	}
+
+}
